3a87489fac9efac823357e563dc40e00711649f0
Khan <amirkhan60972@gmail.com>
2022-11-03 00:32:31 +0530

    more updates

--- app.py
+++ app.py
@@ -22,17 +22,17 @@ def predict():
         Ozone= int(request.form["Ozone"])
         prediction= int(model.predict([[PM25,PM100,NO2,NH3,SO2,CO,Ozone]]))
         if prediction<=50:
-            return render_template("index.html",prediction_text="AQI= {} Good".format(prediction))
+            return render_template("index.html",prediction_text="AQI= {}".format(prediction),status="Status: Good")
         elif prediction>50 and prediction<=100:
-            return render_template("index.html",prediction_text="AQI= {} Satisfactory".format(prediction))
+            return render_template("index.html",prediction_text="AQI= {}".format(prediction),status="Status: Satisfactory")
         elif prediction>100 and prediction<=200:
-            return render_template("index.html",prediction_text="AQI= {} Moderate".format(prediction))
+            return render_template("index.html",prediction_text="AQI= {}".format(prediction),status="Status: Moderate")
         elif prediction>200 and prediction<=300:
-            return render_template("index.html",prediction_text="AQI= {} Poor".format(prediction))
+            return render_template("index.html",prediction_text="AQI= {}".format(prediction),status="Status: Poor")
         elif prediction>300 and prediction<=400:
-            return render_template("index.html",prediction_text="AQI= {} Very Poor".format(prediction))
+            return render_template("index.html",prediction_text="AQI= {}".format(prediction),status="Status: Very Poor")
         elif prediction>400:
-            return render_template("index.html",prediction_text="AQI= {} Severe".format(prediction))
+            return render_template("index.html",prediction_text="AQI= {}".format(prediction),status="Status: Severe")
     else:
         return render_template("index.html")
 if __name__=="__main__":
